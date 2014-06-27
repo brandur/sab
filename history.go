474cd32adb07284232582cf80a8267df13c33f9a
Brandur <brandur@mutelight.org>
2014-06-27 15:30:44 -0700

    Add number to history

--- history.go
+++ history.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	flag "github.com/ogier/pflag"
 )
+
+var (
+	numHistories *int
+)
+
+func init() {
+	numHistories = flag.IntP("num", "n", 10, "number of histories to fetch")
+}
 
 func runHistory() {
 	needApiKey()
 
 	client := newSabClient(*url, *apiKey)
-	histories, err := client.getHistories(10)
+	histories, err := client.getHistories(*numHistories)
 	if err != nil {
 		printFatal("api request: %v", err.Error())
 	}
